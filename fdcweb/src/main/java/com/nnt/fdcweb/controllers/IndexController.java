164ba925d745b696577a9b4a3d736744e9d1b93f
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2024-10-29 19:09:01 +0700

    fix requets index

--- fdcweb/src/main/java/com/nnt/fdcweb/controllers/IndexController.java
+++ fdcweb/src/main/java/com/nnt/fdcweb/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.nnt.fdcweb.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showIndex() {
         return "index";
     }
