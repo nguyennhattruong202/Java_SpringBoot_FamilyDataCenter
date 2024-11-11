a3024a4fb94a2ded45db458eee0b6a5123d115b8
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2024-11-11 22:42:29 +0700

    update code

--- fdcweb/src/main/java/com/nnt/fdcweb/dto/request/UserRequest.java
+++ fdcweb/src/main/java/com/nnt/fdcweb/dto/request/UserRequest.java
@@ -17,29 +17,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserRequest {
 
-    @NotNull(message = "Fullname is not null")
-    @Size(max = 200, message = "Fullname max size is 200 character")
     private String fullname;
-    @NotBlank(message = "Gender is not empty")
-    @NotNull(message = "Gender is not null")
-    @Size(max = 10, message = "Gender max size is 10 character")
     private String gender;
-    @NotNull(message = "Date of birth is not null")
     private LocalDate dateOfBirth;
-    @NotBlank(message = "Phone is not empty")
-    @NotNull(message = "Phone is not null")
-    @Size(max = 20, message = "Phone max size is 20 character")
     private String phone;
-    @Size(max = 200, message = "Email max size is 200 character")
-    @Email(message = "Invalid email")
     private String email;
-    @Size(max = 20, message = "ID max size is 20 character")
     private String ID;
-    @Size(max = 200, message = "Address max size is 200 character")
     private String address;
-    @NotBlank(message = "Password is not empty")
-    @NotNull(message = "Password is not null")
-    @Size(min = 8, max = 100, message = "Password must be at least 8 characters and maximum 100 characters")
     private String password;
     private String note;
 }
