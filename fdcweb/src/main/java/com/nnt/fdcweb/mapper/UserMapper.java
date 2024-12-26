2cde04cd08871532e690cc68732148bf81ba2ddb
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2024-12-26 13:31:23 +0700

    add mappper

--- fdcweb/src/main/java/com/nnt/fdcweb/mapper/UserMapper.java
+++ fdcweb/src/main/java/com/nnt/fdcweb/mapper/UserMapper.java
@@ -1,10 +1,31 @@
 package com.nnt.fdcweb.mapper;
 
+import com.nnt.fdcweb.dto.request.CreateAccountRequest;
 import com.nnt.fdcweb.dto.request.UserRequest;
+import com.nnt.fdcweb.dto.response.CreateAccountResponse;
 import com.nnt.fdcweb.dto.response.UserResponse;
 import com.nnt.fdcweb.entity.User;
 
 public class UserMapper {
+
+    public static User toUser(CreateAccountRequest createAccountRequest) {
+        User user = new User();
+        user.setFullname(createAccountRequest.getFullname());
+        user.setGender(createAccountRequest.getGender());
+        user.setDateOfBirth(createAccountRequest.getDateOfBirth());
+        user.setPhone(createAccountRequest.getPhone());
+        return user;
+    }
+
+    public static CreateAccountResponse toCreateAccountResponse(User user) {
+        CreateAccountResponse createAccountResponse = new CreateAccountResponse();
+        createAccountResponse.setId(user.getId());
+        createAccountResponse.setFullname(user.getFullname());
+        createAccountResponse.setGender(user.getGender());
+        createAccountResponse.setDateOfBirth(user.getDateOfBirth());
+        createAccountResponse.setPhone(user.getPhone());
+        return createAccountResponse;
+    }
 
     public static User toUser(UserRequest userRequest) {
         User user = new User();
